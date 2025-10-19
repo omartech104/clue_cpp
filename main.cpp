b59b3d1d8d458786b0a1fa5c206925a96804dda3
Omar Mohamed <hssnomr2022@gmail.comm>
2025-10-19 17:55:20 +0300

    using the command curl instead of httplib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-#include "httplib.h"
+#include <cstdlib> 
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	
+	string command = "";
+	cout << "Enter the command you want to know about: ";
+	cin >> command;
 	return 0;
 }
