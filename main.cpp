6a437a8a8b3ca73c5094fd28eb2b850b1ad5143b
Omar Mohamed <hssnomr2022@gmail.comm>
2025-10-19 18:02:22 +0300

    adding command input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> 
+#include <cstdlib>
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	string command = "";
-	cout << "Enter the command you want to know about: ";
-	cin >> command;
-	return 0;
+    string command = "";
+    cout << "Enter the command you want to know about: ";
+    cin >> command;
+    string chst = "curl http://cht.sh/" + command;
+    system(chst.c_str());
+    return 0;
 }
+
